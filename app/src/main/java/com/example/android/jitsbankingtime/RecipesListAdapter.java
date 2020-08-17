8ffe663ed8cfb0b7c8e1380e6031d666568b2853
jharnam <jgulrajani@babbel.com>
2020-08-17 12:38:27 +0200

    added data binding to the item (recipe) of main list

--- app/src/main/java/com/example/android/jitsbankingtime/RecipesListAdapter.java
+++ app/src/main/java/com/example/android/jitsbankingtime/RecipesListAdapter.java
@@ -1,14 +1,15 @@
 package com.example.android.jitsbankingtime;
 
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.android.jitsbankingtime.databinding.ItemMainRecipeBinding;
 import com.example.android.jitsbankingtime.model.Recipe;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,18 +23,18 @@ public class RecipesListAdapter extends RecyclerView.Adapter<RecipesListAdapter.
     @Override
     public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View recipesView = layoutInflater.inflate(R.layout.item_main_recipe, parent, false);
-
-        return new RecipeViewHolder(recipesView);
+        ItemMainRecipeBinding binding = DataBindingUtil.inflate(layoutInflater,
+                R.layout.item_main_recipe, parent, false);
+        //return new RecipeViewHolder(binding.getRoot());
+        return new RecipeViewHolder(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecipeViewHolder holder, int position) {
         Recipe currentRecipe = recipesList.get(position);
-        Timber.d("currentRecipeName is: " + currentRecipe.getName());
-        holder.recipeName.setText(currentRecipe.getName());
-        holder.numberOfServings.setText(String.valueOf(currentRecipe.getServings()) + " servings");
+        Timber.d("currentRecipeName is: %s", currentRecipe.getName());
 
+        holder.bind(currentRecipe);
 
     }
 
@@ -49,6 +50,8 @@ public class RecipesListAdapter extends RecyclerView.Adapter<RecipesListAdapter.
         Timber.d("inside setRecipesList");
         //TODO - check if the following addAll is better
         //recipesList.addAll(recipesListForAdapter);
+        //TODO - test this - update MainActivity to remove the clearList call from there
+        //recipesList.clear();
         recipesList = recipesListForAdapter;
         notifyDataSetChanged();
     }
@@ -61,15 +64,55 @@ public class RecipesListAdapter extends RecyclerView.Adapter<RecipesListAdapter.
     }
 
     public class RecipeViewHolder extends RecyclerView.ViewHolder {
-        final TextView recipeName;
-        final TextView numberOfServings;
+        /** This field is used for data binding */
+        private ItemMainRecipeBinding binding;
 
 
-        public RecipeViewHolder(@NonNull View itemView) {
-            super(itemView);
-            recipeName = itemView.findViewById(R.id.recipe_title);
-            numberOfServings = itemView.findViewById(R.id.recipe_servings);
+        //public RecipeViewHolder(@NonNull View itemView) {
+            //super(itemView);
+        //recipeName = itemView.findViewById(R.id.recipe_title);
+        //numberOfServings = itemView.findViewById(R.id.recipe_servings);
+        //itemView.setOnClickListener(this);
+        //}
+
+
+        public RecipeViewHolder(ItemMainRecipeBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+
             //itemView.setOnClickListener(this);
         }
+
+
+        /* This method takes a Recipe object and uses that recipe to display
+        the appropriate information within a list item.
+         */
+        private void bind(Recipe recipe) {
+            if (recipe == null)
+            {
+                Timber.e("Recipe is null :(");
+                return;
+            }
+
+            binding.recipeTitle.setText(recipe.getName());
+
+            String imageUrl = recipe.getImage();
+            setImage(imageUrl);
+
+            binding.recipeServings.setText(String.valueOf(recipe.getServings()) + " servings");
+
+        }
+
+        private void setImage(String url) {
+            if (url.isEmpty() || url.trim().equals("")) {
+                //use a default image
+                binding.recipeThumbnail.setImageResource(R.drawable.recipe_placeholder_icon);
+            } else {
+                Picasso.get()
+                        .load(url)
+                        .into(binding.recipeThumbnail);
+            }
+        }
+
     }
 }
