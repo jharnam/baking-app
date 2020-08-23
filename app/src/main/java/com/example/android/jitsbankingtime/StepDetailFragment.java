60be459f3489e973c67883d2e0bd0e2a7bcf0be7
jharnam <jgulrajani@babbel.com>
2020-08-23 21:47:07 +0200

    videoplayer works - for the 1st time it is opened for a recipe

--- app/src/main/java/com/example/android/jitsbankingtime/StepDetailFragment.java
+++ app/src/main/java/com/example/android/jitsbankingtime/StepDetailFragment.java
@@ -1,5 +1,7 @@
 package com.example.android.jitsbankingtime;
 
+import android.annotation.SuppressLint;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,14 +18,45 @@ import androidx.fragment.app.FragmentManager;
 import com.example.android.jitsbankingtime.databinding.FragmentStepDetailBinding;
 import com.example.android.jitsbankingtime.model.Recipe;
 import com.example.android.jitsbankingtime.model.Step;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.RenderersFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import java.util.Objects;
 
+import static android.view.View.GONE;
+import static com.example.android.jitsbankingtime.utils.ConstantsDefined.APP_NAME;
+
 public class StepDetailFragment extends Fragment {
-    FragmentStepDetailBinding binding;
-    Step step;
-    int currentStepId;
-    Recipe recipe;
+    private FragmentStepDetailBinding binding;
+    private Step step;
+    private int currentStepId;
+    private Recipe recipe;
+
+    //TODO private ExoPlayer mediaPlayer;
+    private SimpleExoPlayer mediaPlayer;
+    private PlaybackStateListener playbackStateListener;
+    private boolean playWhenReady = true;
+    private int currentWindow = 0;
+    private long playbackPosition = 0;
+    private boolean containsVideo = false;
+    private String videoUrl;
+    private String thumbnailUrl;
 
     //required empty constructor
     public StepDetailFragment() {
@@ -41,7 +74,6 @@ public class StepDetailFragment extends Fragment {
         //inflate the fragment layout
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_step_detail, container, false);
 
-        //TODO should we call this again?
         populateRecipeAndStepDetails();
 
 
@@ -54,6 +86,9 @@ public class StepDetailFragment extends Fragment {
 
         //handler for next button click
         onNextButtonClick();
+
+        //playback state listeners
+        playbackStateListener = new PlaybackStateListener();
 
         return binding.getRoot();
     }
@@ -108,9 +143,202 @@ public class StepDetailFragment extends Fragment {
         if (step == null) {
             step = ((StepDetailActivity) Objects.requireNonNull(getActivity())).getStep();
             currentStepId = step.getId();
+            videoUrl = step.getVideoURL();
+            thumbnailUrl = step.getThumbnailURL();
+            if (videoUrl.isEmpty()) {
+                //check if thumbailUrl has an "mp4"
+                if (!(thumbnailUrl.isEmpty())) {
+                    if (thumbnailUrl.contains("mp4")) {
+                        //this can be used instead of videoUrl
+                        containsVideo = true;
+                        videoUrl = thumbnailUrl;
+                    } else {
+                        //its a normal thumbail :)
+                        containsVideo = false;
+                        binding.playerView.setVisibility(GONE);
+
+                        //display it using picasso
+                        //TODO
+                    }
+                } else {
+                    //thumbnail is also empty
+                    containsVideo = false;
+                    binding.playerView.setVisibility(GONE);
+
+                    //display default image using Picasso
+                    //TODO
+                    binding.imageViewStepDefault.setImageResource(R.drawable.recipe_placeholder_icon);
+                }
+            } else {
+                containsVideo = true;
+            }
         }
 
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideSystemUi();
+        if ((Util.SDK_INT <= 23 || mediaPlayer == null)) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
+
+
+    private void initializePlayer() {
+        if (containsVideo) {
+            if (mediaPlayer == null) {
+                // Create an instance of the ExoPlayer.
+                DefaultRenderersFactory defaultRenderersFactory = new DefaultRenderersFactory(this.getContext());
+                TrackSelector trackSelector = new DefaultTrackSelector();
+                LoadControl loadControl = new DefaultLoadControl();
+                //TODO mediaPlayer = SimpleExoPlayer.Builder(this.getContext()).build();
+                mediaPlayer = ExoPlayerFactory.newSimpleInstance(
+                        (RenderersFactory) defaultRenderersFactory, trackSelector, loadControl);
+
+                //mediaPlayer = ExoPlayerFactory.newSimpleInstance(this.getContext(),
+                //        new DefaultTrackSelector());
+                //mediaPlayer = SimpleExoPlayer.Builder(this).build;
+
+                binding.playerView.setPlayer(mediaPlayer);
+
+                //TODO
+                //mediaPlayer.setPlayWhenReady();
+            }
+
+            // Set the ExoPlayer.EventListener to this fragment.
+            mediaPlayer.addListener(playbackStateListener);
+
+            // Prepare the MediaSource.
+            Uri mediaUri = Uri.parse(videoUrl);
+            String userAgent = Util.getUserAgent(this.getContext(), APP_NAME);
+
+            //MediaSource mediaSource = new ExtractorMediaSource.Factory(
+            //        new DefaultDataSourceFactory(this.getContext(), userAgent))
+            //        .createMediaSource(mediaUri);
+            MediaSource mediaSource = new ExtractorMediaSource.Factory(
+                    new DefaultHttpDataSourceFactory(userAgent))
+                    .createMediaSource(mediaUri);
+
+            mediaPlayer.prepare(mediaSource);
+            mediaPlayer.setPlayWhenReady(true);
+        }
+    }
+
+
+    /**
+     * Release ExoPlayer.
+     */
+    private void releasePlayer() {
+        //TODO mNotificationManager.cancelAll();
+        if (mediaPlayer != null) {
+            playbackPosition = mediaPlayer.getCurrentPosition();
+            currentWindow = mediaPlayer.getCurrentWindowIndex();
+            playWhenReady = mediaPlayer.getPlayWhenReady();
+            //TODO mediaPlayer.stop();
+
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    @SuppressLint("InlinedApi")
+    private void hideSystemUi() {
+        /*
+        binding.playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+         */
+    }
+
+    private class PlaybackStateListener implements Player.EventListener {
+        //Player.EventListener methods
+        @Override
+        public void onTimelineChanged(Timeline timeline, Object manifest, int reason) {
+
+        }
+
+        @Override
+        public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+        }
+
+        @Override
+        public void onLoadingChanged(boolean isLoading) {
+
+        }
+
+        /**
+         * Method that is called when the ExoPlayer state changes. Used to update the MediaSession
+         * PlayBackState to keep in sync, and post the media notification.
+         *
+         * @param playWhenReady true if ExoPlayer is playing, false if it's paused.
+         * @param playbackState int describing the state of ExoPlayer. Can be STATE_READY, STATE_IDLE,
+         *                      STATE_BUFFERING, or STATE_ENDED.
+         */
+        @Override
+        public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+
+        }
+
+        @Override
+        public void onRepeatModeChanged(int repeatMode) {
+
+        }
+
+        @Override
+        public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
+
+        }
+
+        @Override
+        public void onPlayerError(ExoPlaybackException error) {
+
+        }
+
+        @Override
+        public void onPositionDiscontinuity(int reason) {
+
+        }
+
+        @Override
+        public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+        }
+
+        @Override
+        public void onSeekProcessed() {
+
+        }
+
+    }
 }
--- app/src/main/java/com/example/android/jitsbankingtime/utils/ConstantsDefined.java
+++ app/src/main/java/com/example/android/jitsbankingtime/utils/ConstantsDefined.java
@@ -2,6 +2,8 @@ package com.example.android.jitsbankingtime.utils;
 
 public final class ConstantsDefined {
 
+        public static final String APP_NAME = "Baking Time";
+
         public static final String BASE_URL = "https://d17h27t6h515a5.cloudfront.net/";
         public static final String URL_PARAMS = "topher/2017/May/59121517_baking/baking.json";
 
