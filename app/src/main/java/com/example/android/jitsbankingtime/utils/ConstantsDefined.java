ff576bfe4457137eddc44a9581e631d9ab0aa936
jharnam <jgulrajani@babbel.com>
2020-08-24 10:08:35 +0200

    saveInstateState, start of MediaSessionCallback

--- app/src/main/java/com/example/android/jitsbankingtime/utils/ConstantsDefined.java
+++ app/src/main/java/com/example/android/jitsbankingtime/utils/ConstantsDefined.java
@@ -8,11 +8,27 @@ public final class ConstantsDefined {
         public static final String URL_PARAMS = "topher/2017/May/59121517_baking/baking.json";
 
         /**
-         * Extra-Tag for the recipe, step to be sent/received in the intent
+         * Extra-Tags for the recipe, step to be sent/received in the intent
          */
         public static final String EXTRA_RECIPE = "recipe";
         public static final String EXTRA_STEP = "step";
         public static final String EXTRA_STEP_ID = "stepId";
+
+        /**
+         * Tags for the saving instance state in StepDetailFragment
+         */
+        public static final String SAVE_RECIPE = "save_recipe";
+        public static final String SAVE_STEP = "save_step";
+        public static final String SAVE_CURRENT_STEP_ID = "save_current_step_id";
+        public static final String SAVE_PLAYBACK_POSITION = "save_playback_position";
+        public static final String SAVE_CURRENT_WINDOW = "save_current_window";
+        public static final String SAVE_PLAY_WHEN_READY = "save_play_when_ready";
+
+
+        /**
+         * EXOPLAYER CONSTANTS
+         */
+        public static final int REWIND_FAST_FORWARD_INCREMENT = 3000;
 
 
         /**
